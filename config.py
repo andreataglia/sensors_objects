a52b9ddcd0c7c3e9d47c0537dca63168e17cab6b
andreataglia <drew.taglia@gmail.com>
2018-11-03 20:05:56 +0800

    works very good with door in my apt

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from array import *
 
 #input file and ouput file
-DATA_IN_PATH = "raw_dataset/first10Cleaned.json"
-DATA_OUT_PATH = "data/data_out_full.csv"
+DATA_IN_PATH = "raw_dataset/andreaDataCleaned.json"
+DATA_OUT_PATH = "data/data_val.csv"
 
 #values to keep in the outfile
-OBJECTS = [["door_outdoor", 1],["door_indoor", 1],["window_indoor", 2],["window_outdoor", 2]] #object mapped with label to be used on the output file
+OBJECTS = [["door_apt_in", 1], ["door_room_in", 1]] #object mapped with label to be used on the output file
 MEASURES = ["accel"] #accel, gyro, magnto, temp(which gets the tuple pressure, temp, light)
